0b6b6d36a0f8d09bf8f3d68453eb366f31efed7f
Chris Hoover <cmhoove14@gmail.com>
2019-04-04 16:27:53 -0700

    Script to download 2016, 2017, and 2018 SNODAS data

---
+++ scripts/snow/get_snodas_vars_2016_through_2018.R
@@ -0,0 +1,28 @@
+source("scripts/snow/snodas_functions.R")
+
+require(lubridate)
+require(googledrive)
+require(raster)
+require(rwrfhydro)
+
+#Get all snodas surfaces from 2015
+
+dates_2016_2018 <- seq(ymd("2016-01-01"), ymd("2018-12-31"), "days")
+
+#Snow Depth
+  for(i in 1:length(dates_2016_2018)){
+    write_snodas_to_GoogleDrive(dates_2016_2018[i],
+                                var_name = "snowDepth", 
+                                temp_directory = normalizePath("scripts/snow/temp_snow"),
+                                GDfolder_id = "1_IxGme096iUx6JJQY0nhONKSzBWaiI3k")
+    print(i)
+  }
+
+#Snow water equivalent
+  for(i in 1:length(dates_2016_2018)){
+    write_snodas_to_GoogleDrive(dates_2016_2018[i],
+                                var_name = "SWE", 
+                                temp_directory = normalizePath("scripts/snow/temp_snow"),
+                                GDfolder_id = "1JPXf6Pq9Ki9zjTSctvO2UxfRn2KTUiq4")
+    print(i)
+  }
