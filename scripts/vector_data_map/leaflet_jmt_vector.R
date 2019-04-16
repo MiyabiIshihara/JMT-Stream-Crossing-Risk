0e8e5f70b251f63791b49c9380b4a5c4e5f30c35
Chris Hoover <cmhoove14@gmail.com>
2019-04-16 13:45:37 -0700

    Convert sp objects to sf, add popup field for crossings, but still need to fill popup field with relevant info

--- scripts/vector_data_map/leaflet_jmt_vector.R
+++ scripts/vector_data_map/leaflet_jmt_vector.R
@@ -7,7 +7,8 @@ require(htmltools)
 # Get vector data from google drive to plot ###############
   # Actual JMT trail vector and entry trails
     jmt_trail <- load_rgdal_from_googledrive("1RJPvOwVY1mcfjKh1eVybZHfWuIX1jE23", "Trail Edges")
-      jmt_trail <- spTransform(jmt_trail, "+proj=longlat +ellps=GRS80 +no_defs")
+      jmt_trail <- spTransform(jmt_trail, "+proj=longlat +ellps=GRS80 +no_defs") %>% st_as_sf() %>% 
+        st_transform(crs = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
       
   #Subset trails to main JMT or access trails    
     jmt_access <- jmt_trail[which(jmt_trail$Type == "Access"),]
@@ -17,14 +18,15 @@ require(htmltools)
     jmt_crossings <- load_rgdal_from_googledrive("1klB4m5GQVIv7sVaZnZzbbomnkqpfDih2")
       jmt_crossings_simplify <- data.frame(lon = jmt_crossings@coords[,1], 
                                            lat = jmt_crossings@coords[,2], 
-                                           Crossing = as.character(jmt_crossings$JMT_Cross))
-
-  # Stream network
-    
+                                           Crossing = as.character(jmt_crossings$JMT_Cross),
+                                           popup_field = as.character(jmt_crossings$Down_Name)) %>% 
+        st_as_sf(coords = c("lon", "lat")) %>% 
+        st_set_crs("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
     
   # Watersheds upstream of stream crossings  
     jmt_watersheds <- load_rgdal_from_googledrive("1yB7ww8YgWCAOHjeuCa4Xu6vIZPthO3aD")
-      jmt_watersheds <- spTransform(jmt_watersheds, "+proj=longlat +ellps=GRS80 +no_defs")
+      jmt_watersheds <- spTransform(jmt_watersheds, "+proj=longlat +ellps=GRS80 +no_defs") %>% st_as_sf() %>% 
+        st_transform(crs = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
 
 # Make icon for stream crossings ############      
 crossingIcon <- makeIcon(
@@ -41,10 +43,9 @@ crossingIcon <- makeIcon(
                  weight = 2, opacity = 0.75) %>% 
     addPolylines(data = jmt_main, color = "brown", label = ~htmlEscape(Name),
                  weight = 4, opacity = 0.9) %>%
-    addMarkers(~lon, ~lat, label = ~htmlEscape(Crossing), icon = ~crossingIcon,
-               data = jmt_crossings_simplify) %>% 
-    #addPopups(~lon, ~lat, popup = ~htmlEscape(Crossing),
-    #           data = jmt_crossings_simplify) %>%    
+    addMarkers(data = jmt_crossings_simplify, 
+               label = ~htmlEscape(Crossing), icon = ~crossingIcon,
+               popup = ~htmlEscape(popup_field)) %>% 
     addPolygons(data = jmt_watersheds, color = "blue",
                 opacity = 0.9, weight = 1, fillOpacity = 0.2)
   
