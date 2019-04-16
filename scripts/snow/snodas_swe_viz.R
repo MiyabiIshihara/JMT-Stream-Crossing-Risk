bd1c0446c9b1aab8de48140871fe97ed65119026
Chris Hoover <cmhoove14@gmail.com>
2019-04-16 13:05:31 -0700

    Add estimates of oncrease in risk score associated with SWE melt

--- scripts/snow/snodas_swe_viz.R
+++ scripts/snow/snodas_swe_viz.R
@@ -6,11 +6,16 @@ require(raster)
 require(tidyverse)
 
 #Get time series of snodas data in JMT watersheds from GoogleDrive
-jmt_swe_2015 <- load_csv_from_googledrive("1Py8svcq-YMBUlhKYJR_HnfVGf41dkFyp")
-jmt_swe_2016 <- load_csv_from_googledrive("15vpYxV75PpQWGEmhEoTkSvkx07vp5H5P")
-jmt_swe_2017 <- load_csv_from_googledrive("1bjlEhCZ9ghEoLtUXmjiOJQKkxKDOk4zS")
-jmt_swe_2018 <- load_csv_from_googledrive("1OwX0bljESiE6UCDO3m6J_NlRGlkPpQ9a")
+  jmt_swe_2015 <- load_csv_from_googledrive("1Py8svcq-YMBUlhKYJR_HnfVGf41dkFyp")
+  jmt_swe_2016 <- load_csv_from_googledrive("15vpYxV75PpQWGEmhEoTkSvkx07vp5H5P")
+  jmt_swe_2017 <- load_csv_from_googledrive("1bjlEhCZ9ghEoLtUXmjiOJQKkxKDOk4zS")
+  jmt_swe_2018 <- load_csv_from_googledrive("1OwX0bljESiE6UCDO3m6J_NlRGlkPpQ9a")
 
+#Get risk model object for estimate of SEW-driven change in risk
+  load("scripts/risk_model_object.Rdata")
+  
+  swe_change_coef <- crossing_difficulty_lin_mod_adj$coefficients["peak_SWE_melt"]
+  
 #2015 #########
 # Visualization of total SWE in each watershed over time for 2015 
   jmt_swe_2015 %>% 
@@ -36,6 +41,22 @@ ggsave(filename = "Plots_Viz/SWE_Time_Series/SWE_time_series_2015.png", width = 
       labs(title = "Daily Change in SWE in JMT watersheds, 2015")
 
 ggsave(filename = "Plots_Viz/SWE_Time_Series/SWE_melt_time_series_2015.png", width = 10, height = 6)
+
+# Visualization of SWE melt associated risk in each watershed over time
+  jmt_swe_2015 %>% 
+    gather("watershed", "SWE", -Date) %>% 
+    group_by(watershed) %>% 
+    mutate(last_swe = dplyr::lag(SWE, order_by = watershed),
+           SWE_melt = -(SWE - last_swe),
+           melt_risk = if_else(SWE_melt * swe_change_coef > 0, SWE_melt * swe_change_coef, 0)) %>% 
+    ggplot(aes(x = Date, y = melt_risk, col = watershed)) +
+      geom_line(size = 1.2) +
+      theme_classic() +
+      theme(legend.position = "bottom") +
+      labs(title = "SWE melt associated risk score in JMT watersheds, 2015",
+           y = "Predicted change in reported risk score")
+  
+ggsave(filename = "Plots_Viz/SWE_Time_Series/SWE_melt_risk_time_series_2015.png", width = 10, height = 6)
   
 #2016 ######################  
 # Visualization of total SWE in each watershed over time for 2016 
@@ -63,6 +84,22 @@ ggsave(filename = "Plots_Viz/SWE_Time_Series/SWE_time_series_2016.png", width = 
   
 ggsave(filename = "Plots_Viz/SWE_Time_Series/SWE_melt_time_series_2016.png", width = 10, height = 6)
   
+# Visualization of SWE melt associated risk in each watershed over time
+  jmt_swe_2016 %>% 
+    gather("watershed", "SWE", -Date) %>% 
+    group_by(watershed) %>% 
+    mutate(last_swe = dplyr::lag(SWE, order_by = watershed),
+           SWE_melt = -(SWE - last_swe),
+           melt_risk = if_else(SWE_melt * swe_change_coef > 0, SWE_melt * swe_change_coef, 0)) %>% 
+    ggplot(aes(x = Date, y = melt_risk, col = watershed)) +
+      geom_line(size = 1.2) +
+      theme_classic() +
+      theme(legend.position = "bottom") +
+      labs(title = "SWE melt associated risk score in JMT watersheds, 2016",
+           y = "Predicted change in reported risk score")
+  
+ggsave(filename = "Plots_Viz/SWE_Time_Series/SWE_melt_risk_time_series_2016.png", width = 10, height = 6)
+
 # 2017 #############
 # Visualization of total SWE in each watershed over time for 2015 
   jmt_swe_2017 %>% 
@@ -88,6 +125,22 @@ ggsave(filename = "Plots_Viz/SWE_Time_Series/SWE_time_series_2017.png", width = 
       labs(title = "Daily Change in SWE in JMT watersheds, 2017")
   
 ggsave(filename = "Plots_Viz/SWE_Time_Series/SWE_melt_time_series_2017.png", width = 10, height = 6)
+
+# Visualization of SWE melt associated risk in each watershed over time
+  jmt_swe_2017 %>% 
+    gather("watershed", "SWE", -Date) %>% 
+    group_by(watershed) %>% 
+    mutate(last_swe = dplyr::lag(SWE, order_by = watershed),
+           SWE_melt = -(SWE - last_swe),
+           melt_risk = if_else(SWE_melt * swe_change_coef > 0, SWE_melt * swe_change_coef, 0)) %>% 
+    ggplot(aes(x = Date, y = melt_risk, col = watershed)) +
+      geom_line(size = 1.2) +
+      theme_classic() +
+      theme(legend.position = "bottom") +
+      labs(title = "SWE melt associated risk score in JMT watersheds, 2017",
+           y = "Predicted change in reported risk score")
+  
+ggsave(filename = "Plots_Viz/SWE_Time_Series/SWE_melt_risk_time_series_2017.png", width = 10, height = 6)
 
 # 2018 ##############  
 # Visualization of total SWE in each watershed over time for 2015 
@@ -115,3 +168,18 @@ ggsave(filename = "Plots_Viz/SWE_Time_Series/SWE_time_series_2018.png", width = 
   
 ggsave(filename = "Plots_Viz/SWE_Time_Series/SWE_melt_time_series_2018.png", width = 10, height = 6)
   
+# Visualization of SWE melt associated risk in each watershed over time
+  jmt_swe_2018 %>% 
+    gather("watershed", "SWE", -Date) %>% 
+    group_by(watershed) %>% 
+    mutate(last_swe = dplyr::lag(SWE, order_by = watershed),
+           SWE_melt = -(SWE - last_swe),
+           melt_risk = if_else(SWE_melt * swe_change_coef > 0, SWE_melt * swe_change_coef, 0)) %>% 
+    ggplot(aes(x = Date, y = melt_risk, col = watershed)) +
+      geom_line(size = 1.2) +
+      theme_classic() +
+      theme(legend.position = "bottom") +
+      labs(title = "SWE melt associated risk score in JMT watersheds, 2018",
+           y = "Predicted change in reported risk score")
+  
+ggsave(filename = "Plots_Viz/SWE_Time_Series/SWE_melt_risk_time_series_2018.png", width = 10, height = 6)
