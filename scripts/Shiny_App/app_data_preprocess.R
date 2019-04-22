c4847416ad0f4f20a98ba92c13272b6d7fd7207e
Chris Hoover <cmhoove14@gmail.com>
2019-04-22 09:35:18 -0700

    Merge separate data pre-processing scripts into one

--- scripts/Shiny_App/app_data_preprocess.R
+++ scripts/Shiny_App/app_data_preprocess.R
@@ -3,6 +3,7 @@ require(leaflet)
 require(sp)
 require(sf)
 require(htmltools)
+require(lubridate)
 require(tidyverse)
 
 source("scripts/googledrive_read_write_functions.R")
@@ -34,6 +35,9 @@ source("scripts/googledrive_read_write_functions.R")
 # Get data frame of googledrive ids for all the snowdepth rasters
   snodas_gd_depth <- drive_ls(as_id("1_IxGme096iUx6JJQY0nhONKSzBWaiI3k"))
       
+#JMT extent  
+  jmt_clipper <- extent(-120, -118, 36, 38)
+      
 #function to get snowdepth geotiff on particular day
 get_snodas_tif <- function(date){
     snodas_id <- snodas_gd_depth %>% 
@@ -44,7 +48,22 @@ get_snodas_tif <- function(date){
 
     return(snodas_data)
 }
+
+# Function to download snowdepth raster and clip it
+  snodas_jmt_clip <- function(date){
+    to_clip <- get_snodas_tif(date)
     
+    clipped <- crop(to_clip, jmt_clipper)
+    
+    return(clipped)
+  }
+  
+# 2015 dates snow depth  
+  dates_2015 <- seq(ymd("2015-01-01"), ymd("2015-12-31"), "days")
+  
+  snow_depth_2015_jmt <- lapply(dates_2015, snodas_jmt_clip)
+  
+  saveRDS(snow_depth_2015_jmt, "scripts/Shiny_App/Data/snow_depth_2015.rds")    
       
 # Make icon for stream crossings ############      
 crossingIcon <- makeIcon(
