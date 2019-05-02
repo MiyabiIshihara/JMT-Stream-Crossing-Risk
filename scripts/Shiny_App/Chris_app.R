3efbd7a144189914cc0397579445d4328d2abea4
Chris Hoover <cmhoove14@gmail.com>
2019-05-02 12:03:53 -0700

    Add data for time series viz, update app.R and Chris_App to Chester's app for updates

--- scripts/Shiny_App/Chris_app.R
+++ scripts/Shiny_App/Chris_app.R
@@ -1,3 +1,6 @@
+
+## app.R ##
+library(shinydashboard)
 library(shiny)
 library(usmap)
 library(ggplot2)
@@ -8,79 +11,470 @@ library(viridis)
 library(lubridate)
 library(tidyverse)
 library(htmltools)
+library(dplyr)
+library(markdown)
 
+# colors <- list(
+#   logo = "",
+#   logoHover = "",
+#   collapseButton = "",
+#   collapseButtonHover = "",
+#   topBar = "",
+#   sideBar = "",
+#   
+#   
+# )
+
+# ------------------------------- # 
+#         import data             # 
+# ------------------------------- # 
 # Load data ###########
-  jmt_crossings_simplify <- readRDS("Data/crossing_points.rds")
-  jmt_main <- readRDS("Data/jmt_main_trail.rds")
-  jmt_access <- readRDS("Data/jmt_access_trails.rds")
-  jmt_watersheds <- readRDS("Data/jmt_watersheds.rds")
-  
-  snow_depth_2015_jmt <- readRDS("Data/snow_depth_2015.rds")
-  
+jmt_crossings_simplify <- readRDS("Data/crossing_points.rds")
+jmt_all <- readRDS("Data/jmt_all_trail.rds")
+jmt_main <- jmt_all %>% filter(Type == 'Main')
+jmt_access <- jmt_all %>% filter(Type == 'Access')
+jmt_watersheds <- readRDS("Data/jmt_watersheds.rds")
+snow_depth_2015_jmt <- readRDS("Data/snow_depth_2015.rds")
+precip_2015_jmt <- readRDS("Data/prism_ppt_jmt_clip_2015.rds")
+route_info <- readRDS("Data/route_info.rds")
+
+# Combine multiple data into a list
+data <- list("snow_depth" = snow_depth_2015_jmt, 
+             "precip" = precip_2015_jmt)
+
+
 # Make icon for stream crossings ############      
 crossingIcon <- makeIcon(
   iconUrl = "River_Icon/Artboard 1.png",
   iconRetinaUrl = "River_Icon/Artboard 1@2x.png",
-  iconHeight = 35, iconWidth = 20 
+  # iconHeight = 35,
+  # iconWidth = 20,
+  iconAnchorX = 5,
+  iconAnchorY = 30,
+  popupAnchorX = 6,
+  popupAnchorY = -35
+  
 )
- 
 
-# ----------------- # 
-ui <- pageWithSidebar(
-  headerPanel('Snow Depth'),
-  sidebarPanel(
-    sliderInput(inputId = 'time', label = 'Time Range', 
-                min = as.Date("2015-01-01"), max = as.Date("2015-12-31"), 
-                value = as.Date(c("2015-01-01")), 
-                timeFormat = '%Y-%m-%d')
+
+# ------------------------------- # 
+#         ui components           # 
+# ------------------------------- # 
+
+date_selector <- function(){
+  sliderInput(
+    inputId = 'time', 
+    label = '', 
+    min = as.Date("2015-01-01"), 
+    max = as.Date("2015-12-31"), 
+    value = as.Date(c("2015-05-20")), 
+    timeFormat = '%Y-%m-%d'
+  )
+}
+
+trip_selector <- function(){
+  column(
+    12,
+    fluidRow(
+      column(
+        8,
+        style='padding:0px;',
+        selectInput(
+          inputId = "start_th",
+          label = "Starting Trailhead:",
+          choices = sort(unique(route_info$`entry trailhead`)),
+          selected = "Happy Isles Trailhead"
+        )
+      ),
+      column(
+        4,
+        style='padding:0px;',
+        dateInput(
+          inputId="start_date",
+          label="Start Date:",
+          value=today("PMT")
+        )
+      )
+    ),
+
+    fluidRow(
+      column(
+        8,
+        style='padding:0px;',
+        selectInput(
+          inputId="end_th",
+          label="Ending Trailhead:",
+          choices=sort(unique(route_info$`exit trailhead`)),
+          selected="Whitney Portal"
+        )
+      ),
+      column(
+        4,
+        style='padding:0px;',
+        dateInput(
+          inputId="end_date",
+          label="End Date:",
+          value=(today("PMT") + 21)
+        )
+      )
+    )
+  )
+}
+
+raster_selector <- function(){
+  radioButtons(
+    inputId = "variable", 
+    label = "",
+    choices = c(
+      "Snow Depth" = "snow_depth", 
+      "Precipitation" = "precip"
+    ), 
+    selected = "snow_depth"
+  )
+}
+
+
+
+
+# ------------------------------- # 
+#                ui               # 
+# ------------------------------- # 
+#### Header content #### 
+header <- dashboardHeader(
+  title = "John Muir Trail Hazard",
+  titleWidth = 400)
+
+
+#### Sidebar content #### 
+sidebar <- dashboardSidebar(
+  width = 400,
+
+  # Navigation
+  column(
+    12,
+    style = "background-color:#4372a7;",
+    
+    # Header
+    HTML(
+      markdownToHTML(
+        fragment.only=TRUE,
+        text=c(
+          "### Pages"
+        )
+      )
+    ),
+    
+    # Menu Items
+    sidebarMenu(
+      menuItem(
+        "Stream Crossing Map", 
+        tabName="planning"#,
+        # icon=icon("dashboard")
+      ),
+      menuItem(
+        "Historical Stream Flow", 
+        tabName="risk_cause"#,
+        # icon=icon("th")
+      ),
+      menuItem(
+        "Forecasted Stream Flow", 
+        tabName="current_conditions"
+      ),
+      menuItem(
+        "Stream Crossing Information", 
+        tabName="about_us"
+      ),
+      menuItem(
+        "About This Project", 
+        tabName="about_us"
+      )
+    )
   ),
-  mainPanel(
-    leafletOutput('plot1')
+  
+  # Spacing the same color as the Navigation
+  column(
+    12,
+    style = "background-color:#4372a7;",
+    HTML(
+      markdownToHTML(
+        fragment.only=TRUE, 
+        text=c(
+          "<br>"
+        )
+      )
+    )
+  ),
+  
+  # Spacing without a color
+  column(
+    12,
+    HTML(
+      markdownToHTML(
+        fragment.only=TRUE, 
+        text=c(
+          "<br>"
+        )
+      )
+    )
+  ),
+  
+  # Trip Planner
+  column(
+    12,
+    style = "background-color:#4372a7;",
+    HTML(
+      markdownToHTML(
+        fragment.only=TRUE, 
+        text=c(
+          "### Trip Planner"
+        )
+      )
+    ),
+    date_selector(),
+    trip_selector(),
+    raster_selector()
   )
 )
 
-
-
-# ----------------- # 
-
-server <- function(input, output, session) {
-
-  # subset data 
-  selectedData <- reactive({
-    snow_depth_2015_jmt[[yday(input$time)]]
-  })
-  
-  output$plot1 <- renderLeaflet({
-    pal <- colorNumeric("viridis", domain = c(0,2000),
-           na.color = "transparent")
-  leaflet() %>% 
-    setView(lng = -118.869194, lat = 37.235921, zoom = 8) %>% 
-    addProviderTiles(provider = "Esri.WorldTopoMap") %>% 
-    addRasterImage(selectedData(), colors = pal, opacity = 0.4,
-                   maxBytes = 10 * 1024 * 1024,
-                   group = "Snow Depth") %>% 
-    addLegend(pal = pal, values = values(selectedData()), title = "Snow Depth (mm)") %>% 
-    addPolylines(data = jmt_access, color = "green", label = ~htmlEscape(Name),
-                 weight = 2, opacity = 0.75, 
-                 group = "JMT Access Trails") %>% 
-    addPolylines(data = jmt_main, color = "brown", label = ~htmlEscape(Name),
-                 weight = 4, opacity = 0.9, 
-                 group = "JMT Main Trail") %>%
-    addMarkers(data = jmt_crossings_simplify, 
-               label = ~htmlEscape(Crossing), icon = ~crossingIcon,
-               popup = ~htmlEscape(popup_field),
-               group = "JMT Main Stream Crossings") %>% 
-    addPolygons(data = jmt_watersheds, color = "blue",
-                opacity = 0.9, weight = 1, fillOpacity = 0.2,
-                group = "Main Crossing Watersheds") %>% 
-    addLayersControl(overlayGroups = c("Snow Depth",
-                                       "JMT Access Trails",
-                                       "JMT Main Trail",
-                                       "JMT Main Stream Crossings",
-                                       "Main Crossing Watersheds"))
-  })
-
-  
+main_map <- function(){
+  leafletOutput(
+    "main_map",
+  )
 }
 
+#### Body content ####
+body <- dashboardBody(
+  
+  tabItems(
+    # 1st tab content -- Planning Crossings
+    tabItem(
+      tabName = "planning",
+      main_map()#,
+      # map_option_panel()
+    ),
+    
+    # 2nd tab content -- What causes risk?
+    tabItem(
+      tabName="risk_cause",
+      includeMarkdown("docs/risk_cause.md")
+    ),
+    
+    # 3rd tab content -- Current conditions
+    tabItem(
+      tabName = "current_conditions"
+    ),
+    
+    # 4th tab content -- About Us
+    tabItem(
+      tabName="about_us",
+      includeMarkdown("docs/about_us.md")
+    )
+  ),
+  
+  # Force the map to fill the whole dashboard body
+  tags$style(type = "text/css", "#main_map {height: calc(100vh - 80px) !important;}"),
+  
+  # Custom UI colors
+  tags$head(
+    tags$style(
+      HTML(
+        '
+        /* logo */
+        .skin-blue .main-header .logo {
+        background-color: #173d69;
+        }
+
+        /* logo when hovered */
+        .skin-blue .main-header .logo:hover {
+        background-color: #1a487d;
+        }
+
+        /* navbar (rest of the header) */
+        .skin-blue .main-header .navbar {
+        background-color: #4372a7;
+        }
+
+        /* main sidebar */
+        .skin-blue .main-sidebar {
+        background-color: #173d69;
+        }
+
+        /* active selected tab in the sidebarmenu */
+        .skin-blue .main-sidebar .sidebar .sidebar-menu .active a{
+        background-color: #ffffff;
+        }
+
+        /* other links in the sidebarmenu */
+        .skin-blue .main-sidebar .sidebar .sidebar-menu a{
+        background-color: #598db8;
+        color: #000000;
+        }
+
+        /* other links in the sidebarmenu when hovered */
+        .skin-blue .main-sidebar .sidebar .sidebar-menu a:hover{
+        background-color: #66a1d2;
+        }
+
+        /* toggle button when hovered  */
+        .skin-blue .main-header .navbar .sidebar-toggle:hover{
+        background-color: #173d69;
+        }
+        '
+      )
+    )
+  )
+)
+
+  
+  
+
+
+ui <- dashboardPage(
+  title = "JMT WebApp", 
+  skin = "blue", 
+  header, 
+  sidebar, 
+  body
+)
+
+
+
+# ------------------------------- # 
+#              server             # 
+# ------------------------------- # 
+
+server <- function(input, output) {
+  
+  # subset data
+  selectedData <- reactive({
+    selectedData <- data[[paste0(input$variable)]]
+    selectedData <- selectedData[[yday(input$time)]]
+    selectedData
+  })
+  
+  compileRoute <- function(start, end) {
+    # Get routes with that start and end
+    routes <- route_info %>% filter(`entry trailhead`==start & `exit trailhead`==end)
+    # Identify the shortest of these routes
+    route <- routes %>% filter(length == min(length))
+    # Get the segment indices associated with this route
+    segment_ids <- route$edge_ids
+    segment_ids <- substr(segment_ids, 2, nchar(segment_ids)-1)
+    segment_ids <- as.integer(strsplit(segment_ids, ", ")[[1]])
+    # Add one to account for R indices starting at 1 instead of 0
+    segment_ids <- segment_ids + 1
+    # Select the segments involves with this route
+    segments = jmt_all  %>% slice(segment_ids)
+    # Get the crossing indices associated with this route
+    crossing_ids <- route$streams_crossed
+    crossing_ids <- substr(crossing_ids, 2, nchar(crossing_ids)-1)
+    crossing_ids <- as.integer(strsplit(crossing_ids, ", ")[[1]])
+    # Add one to account for R indices starting at 1 instead of 0
+    crossing_ids <- crossing_ids + 1
+    # Select the crossings involved with this route
+    crossings <- jmt_crossings_simplify %>% slice(crossing_ids)
+    # Return just the segments associated with the route
+    return(list('segments'=segments, 'crossings'=crossings, 'bounds'=st_bbox(segments)))
+  }
+  route <- reactive({compileRoute(input$start_th, input$end_th)})
+  
+  output$main_map <- renderLeaflet({
+    pal <- colorNumeric(
+      # "viridis",
+      palette = colorRamp(c("#FFFFFF", "#014175"), interpolate = "spline"),
+      domain=c(0,3000),
+      na.color="transparent"
+    )
+    
+    leaflet() %>%
+      # Fit to the bounds of the selected route
+      fitBounds(
+        as.numeric(route()$bounds$xmin), 
+        as.numeric(route()$bounds$ymin), 
+        as.numeric(route()$bounds$xmax), 
+        as.numeric(route()$bounds$ymax)) %>%
+      
+      ### Make basemap selector
+      
+      # # Load basemap
+      # addProviderTiles(provider = "Esri.WorldTopoMap") %>%
+      # Load basemap
+      addProviderTiles(provider = providers$OpenStreetMap.Mapnik) %>%
+      # Load raster layers
+      addRasterImage(
+        selectedData(),
+        # colors = pal,
+        colors = pal,
+        opacity = 0.5,
+        maxBytes = 10 * 1024 * 1024,
+        group = "Snow Depth",
+      ) %>%
+      
+      addLegend(
+        pal = pal,
+        values = values(selectedData()),
+        title = "Snow Depth (mm)"
+      ) %>% ## CHANGE
+      
+      # Map access trails
+      addPolylines(
+        data = jmt_access,
+        color = "#202020",
+        label = ~htmlEscape(Name),
+        weight = 1,
+        opacity = 0.9,
+        group = "JMT Access Trails"
+      ) %>%
+      
+      # Map main JMT trail
+      addPolylines(
+        data = jmt_main,
+        color = "#202020",
+        label = ~htmlEscape(Name),
+        weight = 3,
+        opacity = 0.9,
+        group = "JMT Main Trail"
+      ) %>%
+      
+      # Map stream Crossings
+      addMarkers(
+        # data = jmt_crossings_simplify,
+        data = route()$crossings,
+        label = ~htmlEscape(Crossing),
+        icon = ~crossingIcon,
+        popup = ~htmlEscape(popup_field),
+        group = "JMT Main Stream Crossings"
+      ) %>%
+      
+      # Map watersheds
+      addPolygons(
+        data = jmt_watersheds,
+        color = "#253d66",
+        opacity = 0.9,
+        weight = 1,
+        fillOpacity = 0.2,
+        group = "Main Crossing Watersheds"
+      ) %>%
+      
+      # Map selected route
+      addPolylines(
+        data = route()$segments,
+        color = "#126b20",
+        label = ~htmlEscape(Name),
+        weight = 6,
+        opacity = 1,
+        group = "Selected Route"
+      ) %>%
+      
+      addLayersControl(overlayGroups = c("Snow Depth",
+                                         "JMT Access Trails",
+                                         "JMT Main Trail",
+                                         "JMT Main Stream Crossings",
+                                         "Main Crossing Watersheds",
+                                         "Selected Route"))
+  }) # end of leaflet function
+}
+
+
 shinyApp(ui, server)
+
--- scripts/Shiny_App/app_data_preprocess.R
+++ scripts/Shiny_App/app_data_preprocess.R
@@ -5,6 +5,7 @@ require(sf)
 require(raster)
 require(htmltools)
 require(lubridate)
+require(stringr)
 require(tidyverse)
 
 source("scripts/googledrive_read_write_functions.R")
@@ -13,7 +14,8 @@ source("scripts/googledrive_read_write_functions.R")
   # Actual JMT trail vector and entry trails
     jmt_trail <- load_rgdal_from_googledrive("1RJPvOwVY1mcfjKh1eVybZHfWuIX1jE23", "Trail Edges")
       jmt_trail <- spTransform(jmt_trail, "+proj=longlat +ellps=GRS80 +no_defs") %>% st_as_sf() %>% 
-        st_transform(crs = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+        st_transform(crs = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0") %>% 
+        mutate(edge_id = row_number())
       
   #Subset trails to main JMT or access trails    
     jmt_access <- jmt_trail[which(jmt_trail$Type == "Access"),]
@@ -22,16 +24,53 @@ source("scripts/googledrive_read_write_functions.R")
     jmt_main <- jmt_trail[which(jmt_trail$Type == "Main"),]
       saveRDS(jmt_main, "scripts/Shiny_App/Data/jmt_main_trail.rds")  
     
-  # JMT Stream crossing points are processed in crossings_add_field.R
+  # JMT Stream crossing points are extensively processed in crossings_add_field.R, this just for interactive use with other layers created, edited here
+    jmt_crossings <- load_rgdal_from_googledrive("1klB4m5GQVIv7sVaZnZzbbomnkqpfDih2")
+      jmt_crossings_df <- jmt_crossings@data %>% 
+        mutate(crossing_id = row_number())
 
   # Watersheds upstream of stream crossings  
     jmt_watersheds <- load_rgdal_from_googledrive("1yB7ww8YgWCAOHjeuCa4Xu6vIZPthO3aD")
-      jmt_watersheds <- spTransform(jmt_watersheds, "+proj=longlat +ellps=GRS80 +no_defs") %>% st_as_sf() %>% 
+      jmt_watersheds <- spTransform(jmt_watersheds, "+proj=longlat +ellps=GRS80 +no_defs") %>% 
+        st_as_sf() %>% 
         st_transform(crs = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
       
       saveRDS(jmt_watersheds, "scripts/Shiny_App/Data/jmt_watersheds.rds")  
   
+# Trail routes for drop down list
+  trail_routes <- load_csv_from_googledrive("1L4JVtsC1Jz9_T7wFtxNnDyYq5R65Lou_")
+    trailheads <- unique(trail_routes$`entry trailhead`)
+  
+      saveRDS(trail_routes, "scripts/Shiny_App/Data/trail_routes.rds")
+      saveRDS(trailheads, "scripts/Shiny_App/Data/jmt_trailheads.rds")  
 
+# Time series of SWE in watersheds and associated risk for time series viz
+  #Get time series of snodas data in JMT watersheds from GoogleDrive
+  jmt_swe_2015 <- load_csv_from_googledrive("1Py8svcq-YMBUlhKYJR_HnfVGf41dkFyp")
+  jmt_swe_2016 <- load_csv_from_googledrive("15vpYxV75PpQWGEmhEoTkSvkx07vp5H5P")
+  jmt_swe_2017 <- load_csv_from_googledrive("1bjlEhCZ9ghEoLtUXmjiOJQKkxKDOk4zS")
+  jmt_swe_2018 <- load_csv_from_googledrive("1OwX0bljESiE6UCDO3m6J_NlRGlkPpQ9a")
+
+  jmt_swe15_18 <- rbind(jmt_swe_2015, jmt_swe_2016, jmt_swe_2017, jmt_swe_2018) %>% 
+    gather("watershed", "SWE", -Date) %>% 
+    mutate(Year = year(Date)) %>% 
+    group_by(watershed) %>% 
+    mutate(last_swe = dplyr::lag(SWE, order_by = watershed),
+           SWE_melt = -(SWE - last_swe),
+           melt_risk = if_else(SWE_melt * swe_change_coef > 0, SWE_melt * swe_change_coef, 0)) %>% 
+    ungroup() %>% 
+    left_join(jmt_crossings_df %>% 
+                select(JMT_Cross, crossing_id), 
+              by = c("watershed" = "JMT_Cross"))
+  
+    saveRDS(jmt_swe15_18, "scripts/Shiny_App/Data/swe_risk_2015_2018.rds")
+
+#Get risk model object for estimate of SEW-driven change in risk
+  load("scripts/risk_model_object.Rdata")
+  
+  swe_change_coef <- crossing_difficulty_lin_mod_adj$coefficients["peak_SWE_melt"]
+    
+      
 #Download and process snowdepth rasters ##########
 # Get data frame of googledrive ids for all the snowdepth rasters
   snodas_gd_depth <- drive_ls(as_id("1_IxGme096iUx6JJQY0nhONKSzBWaiI3k"))
@@ -65,7 +104,29 @@ get_snodas_tif <- function(date){
   snow_depth_2015_jmt <- lapply(dates_2015, snodas_jmt_clip)
   
   saveRDS(snow_depth_2015_jmt, "scripts/Shiny_App/Data/snow_depth_2015.rds")    
+
+# 2016 dates snow depth  
+  dates_2016 <- seq(ymd("2016-01-01"), ymd("2016-12-31"), "days")
+  
+  snow_depth_2016_jmt <- lapply(dates_2016, snodas_jmt_clip)
+  
+  saveRDS(snow_depth_2016_jmt, "scripts/Shiny_App/Data/snow_depth_2016.rds")    
+
+# 2017 dates snow depth  
+  dates_2017 <- seq(ymd("2017-01-01"), ymd("2017-12-31"), "days")
+  
+  snow_depth_2017_jmt <- lapply(dates_2017, snodas_jmt_clip)
+  
+  saveRDS(snow_depth_2017_jmt, "scripts/Shiny_App/Data/snow_depth_2017.rds")    
+
+# 2018 dates snow depth  
+  dates_2018 <- seq(ymd("2018-01-01"), ymd("2018-12-31"), "days")
+  
+  snow_depth_2018_jmt <- lapply(dates_2018, snodas_jmt_clip)
+  
+  saveRDS(snow_depth_2018_jmt, "scripts/Shiny_App/Data/snow_depth_2018.rds")    
       
+        
 #Get precip data ########
 # Get data frame of googledrive ids for all the snowdepth rasters
   gd_precip <- drive_ls(as_id("16TsvJGV4YNxG6EER2RokJTOZMFurhsX4"))
