075b9b5b001c86c4b9d79b69c1e4c691e745df29
Chester Harvey <chesterharvey@gmail.com>
2019-05-05 08:27:32 -0700

    Changed slider to show crossings

---
+++ Icons.R
@@ -0,0 +1,99 @@
+icon <- function(url, retinaUrl) {
+  return(
+    makeIcon(
+      iconUrl = url,
+      iconRetinaUrl = retinaUrl,
+      shadowUrl = "www/icon_shadow.png",
+      iconHeight = 35,
+      iconWidth = 25,
+      shadowHeight = 35,
+      shadowWidth = 35,
+      iconAnchorX = 12,
+      iconAnchorY = 35,
+      shadowAnchorX = 6,
+      shadowAnchorY = 32,
+      popupAnchorX = 1,
+      popupAnchorY = -35
+    )
+  )
+}
+
+blueIcon <- icon("www/icon.png", "www/icon2x.png")
+redIcon <- icon("www/icon_red.png", "www/icon2x_red.png")
+yellowIcon <- icon("www/icon_yellow.png", "www/icon2x_yellow.png")
+greenIcon <- icon("www/icon_green.png", "www/icon2x_green.png")
+blueIconSemi <- icon("www/icon_semi.png", "www/icon2x_semi.png")
+redIconSemi <- icon("www/icon_red_semi.png", "www/icon2x_red_semi.png")
+yellowIconSemi <- icon("www/icon_yellow_semi.png", "www/icon2x_yellow_semi.png")
+greenIconSemi <- icon("www/icon_green_semi.png", "www/icon2x_green_semi.png")
+
+
+
+
+crossingIcon <- makeIcon(
+  iconUrl = "www/icon.png",
+  iconRetinaUrl = "www/icon2x.png",
+  shadowUrl = "www/icon_shadow.png",
+  # shadowRetinaUrl = "wwwn/icon2x_shadow.png",
+  iconHeight = 35,
+  iconWidth = 25,
+  shadowHeight = 35,
+  shadowWidth = 35,
+  iconAnchorX = 12,
+  iconAnchorY = 35,
+  shadowAnchorX = 6,
+  shadowAnchorY = 32,
+  popupAnchorX = 1,
+  popupAnchorY = -35
+)
+
+crossingIconRed <- makeIcon(
+  iconUrl = "www/icon_red.png",
+  iconRetinaUrl = "www/icon2x_red.png",
+  shadowUrl = "www/icon_shadow.png",
+  # shadowRetinaUrl = "wwwn/icon2x_shadow.png",
+  iconHeight = 35,
+  iconWidth = 25,
+  shadowHeight = 35,
+  shadowWidth = 35,
+  iconAnchorX = 12,
+  iconAnchorY = 35,
+  shadowAnchorX = 6,
+  shadowAnchorY = 32,
+  popupAnchorX = 1,
+  popupAnchorY = -35
+)
+
+crossingIconYellow <- makeIcon(
+  iconUrl = "www/icon_yellow.png",
+  iconRetinaUrl = "www/icon2x_yellow.png",
+  shadowUrl = "www/icon_shadow.png",
+  # shadowRetinaUrl = "wwwn/icon2x_shadow.png",
+  iconHeight = 35,
+  iconWidth = 25,
+  shadowHeight = 35,
+  shadowWidth = 35,
+  iconAnchorX = 12,
+  iconAnchorY = 35,
+  shadowAnchorX = 6,
+  shadowAnchorY = 32,
+  popupAnchorX = 1,
+  popupAnchorY = -35
+)
+
+crossingIconGreen <- makeIcon(
+  iconUrl = "www/icon_green.png",
+  iconRetinaUrl = "www/icon2x_green.png",
+  shadowUrl = "www/icon_shadow.png",
+  # shadowRetinaUrl = "wwwn/icon2x_shadow.png",
+  iconHeight = 35,
+  iconWidth = 25,
+  shadowHeight = 35,
+  shadowWidth = 35,
+  iconAnchorX = 12,
+  iconAnchorY = 35,
+  shadowAnchorX = 6,
+  shadowAnchorY = 32,
+  popupAnchorX = 1,
+  popupAnchorY = -35
+)
--- scripts/Shiny_App/Chester_app.R
+++ scripts/Shiny_App/Chester_app.R
@@ -34,74 +34,7 @@ crossing_positions <- readRDS("Data/crossing_positions.rds")
 data <- list("snow_depth" = snow_depth_2015_jmt, 
              "precip" = precip_2015_jmt)
 
-# Make icons for stream crossings
-crossingIcon <- makeIcon(
-  iconUrl = "www/icon.png",
-  iconRetinaUrl = "www/icon2x.png",
-  shadowUrl = "www/icon_shadow.png",
-  # shadowRetinaUrl = "wwwn/icon2x_shadow.png",
-  iconHeight = 35,
-  iconWidth = 25,
-  shadowHeight = 35,
-  shadowWidth = 35,
-  iconAnchorX = 12,
-  iconAnchorY = 35,
-  shadowAnchorX = 6,
-  shadowAnchorY = 32,
-  popupAnchorX = 1,
-  popupAnchorY = -35
-)
 
-crossingIconRed <- makeIcon(
-  iconUrl = "www/icon_red.png",
-  iconRetinaUrl = "www/icon2x_red.png",
-  shadowUrl = "www/icon_shadow.png",
-  # shadowRetinaUrl = "wwwn/icon2x_shadow.png",
-  iconHeight = 35,
-  iconWidth = 25,
-  shadowHeight = 35,
-  shadowWidth = 35,
-  iconAnchorX = 12,
-  iconAnchorY = 35,
-  shadowAnchorX = 6,
-  shadowAnchorY = 32,
-  popupAnchorX = 1,
-  popupAnchorY = -35
-)
-
-crossingIconYellow <- makeIcon(
-  iconUrl = "www/icon_yellow.png",
-  iconRetinaUrl = "www/icon2x_yellow.png",
-  shadowUrl = "www/icon_shadow.png",
-  # shadowRetinaUrl = "wwwn/icon2x_shadow.png",
-  iconHeight = 35,
-  iconWidth = 25,
-  shadowHeight = 35,
-  shadowWidth = 35,
-  iconAnchorX = 12,
-  iconAnchorY = 35,
-  shadowAnchorX = 6,
-  shadowAnchorY = 32,
-  popupAnchorX = 1,
-  popupAnchorY = -35
-)
-
-crossingIconGreen <- makeIcon(
-  iconUrl = "www/icon_green.png",
-  iconRetinaUrl = "www/icon2x_green.png",
-  shadowUrl = "www/icon_shadow.png",
-  # shadowRetinaUrl = "wwwn/icon2x_shadow.png",
-  iconHeight = 35,
-  iconWidth = 25,
-  shadowHeight = 35,
-  shadowWidth = 35,
-  iconAnchorX = 12,
-  iconAnchorY = 35,
-  shadowAnchorX = 6,
-  shadowAnchorY = 32,
-  popupAnchorX = 1,
-  popupAnchorY = -35
-)
 
 # ------------------------------- # 
 #         ui components           # 
@@ -166,6 +99,35 @@ raster_selector <- function(){
     selected = "snow_depth"
   )
 }
+
+# Define Icons
+icon <- function(url, retinaUrl) {
+  return(
+    makeIcon(
+      iconUrl = url,
+      # iconRetinaUrl = retinaUrl,
+      shadowUrl = "www/icon_shadow.png",
+      iconHeight = 35,
+      iconWidth = 25,
+      shadowHeight = 35,
+      shadowWidth = 35,
+      iconAnchorX = 12,
+      iconAnchorY = 35,
+      shadowAnchorX = 6,
+      shadowAnchorY = 32,
+      popupAnchorX = 1,
+      popupAnchorY = -35
+    )
+  )
+}
+blueIcon <- icon("www/icon.png", "www/icon2x.png")
+redIcon <- icon("www/icon_red.png", "www/icon2x_red.png")
+yellowIcon <- icon("www/icon_yellow.png", "www/icon2x_yellow.png")
+greenIcon <- icon("www/icon_green.png", "www/icon2x_green.png")
+blueIconSemi <- icon("www/icon_semi.png", "www/icon2x_semi.png")
+redIconSemi <- icon("www/icon_red_semi.png", "www/icon2x_red_semi.png")
+yellowIconSemi <- icon("www/icon_yellow_semi.png", "www/icon2x_yellow_semi.png")
+greenIconSemi <- icon("www/icon_green_semi.png", "www/icon2x_green_semi.png")
 
 # ------------------------------- # 
 #                ui               # 
@@ -288,10 +250,6 @@ body <- dashboardBody(
   )
 )
 
-  
-  
-
-
 ui <- dashboardPage(
   title = "JMT WebApp", 
   skin = "blue", 
@@ -308,82 +266,6 @@ ui <- dashboardPage(
 
 server <- function(input, output) {
   
-  # Select raster data based on trip date
-  selectedData <- reactive({
-    map_date = input$start_date# + input$trip_days - 1
-    selectedData <- data[[paste0(input$variable)]]
-    selectedData <- selectedData[[yday(map_date)]]
-    selectedData
-  })
-  
-  # Prepare a custom day selector UI based on the number of trip days
-  output$trip_day_selector <- renderUI({
-    trip_days <- as.numeric(difftime(input$end_date, input$start_date, units = "days")) + 1
-    fluidRow(
-      # sliderTextInput(
-      #   inputId = 'trip_day',
-      #   label = 'Trip Day:',
-      #   choices = day_steps,
-      #   selected = "Stream A",
-      #   grid = TRUE,
-      #   # min = 1,
-      #   # max = trip_days,
-      #   # value = 1,
-      #   width = "100%"
-      #   # step = 1
-      # )
-      sliderInput(
-        inputId = 'trip_day',
-        label = 'Trip Day:',
-        min = 1,
-        max = trip_days,
-        value = 1,
-        width = "100%",
-        step = 1
-      )
-    )
-  })
-  
-  
-  # # Compile the route between start and end trailheads
-  # compileRoute <- function(start, end) {
-  #   # Get shortest routes with that start and end
-  #   route <- route_info %>%
-  #     filter(`entry trailhead`==start & `exit trailhead`==end) %>%
-  #     filter(length == min(length))
-  #   # Get the segment indices associated with this route
-  #   segment_ids <- route$edge_ids
-  #   segment_ids <- substr(segment_ids, 2, nchar(segment_ids)-1)
-  #   segment_ids <- as.integer(strsplit(segment_ids, ", ")[[1]])
-  #   # Add one to account for R indices starting at 1 instead of 0
-  #   segment_ids <- segment_ids + 1
-  #   # Select the segments involves with this route
-  #   segments = jmt_all  %>% slice(segment_ids)
-  #   
-  #   print(segment_ids)
-  #   
-  #   # Get the crossing indices associated with this route
-  #   crossing_ids <- route$streams_crossed
-  #   crossing_ids <- substr(crossing_ids, 2, nchar(crossing_ids)-1)
-  #   crossing_ids <- as.integer(strsplit(crossing_ids, ", ")[[1]])
-  #   # Add one to account for R indices starting at 1 instead of 0
-  #   crossing_ids <- crossing_ids + 1
-  #   # Select the crossings involved with this route
-  #   crossings <- jmt_crossings_simplify %>% slice(crossing_ids)
-  #   # Return just the segments associated with the route
-  #   
-  #   print(crossing_ids)
-  #   
-  #   return(
-  #     list(
-  #       'segments'=segments,
-  #       'crossings'=crossings,
-  #       'bounds'=st_bbox(segments),
-  #       'id'=route$route_id
-  #     )
-  #   )
-  # }
-  
   # Compile the route between start and end trailheads
   compileRoute <- function(start, end) {
     # Get shortest routes with that start and end
@@ -398,35 +280,75 @@ server <- function(input, output) {
     # Select the crossings involved with this route
     crossing_ids <- route$crossing_ids[[1]] + 1
     crossings <- jmt_crossings_simplify %>% slice(crossing_ids)
-    # Return just the segments associated with the route
+    
     return(
       list(
-        'segments'=segments,
-        'crossings'=crossings,
+        'segment_geoms'=segments,
+        'crossing_geoms'=crossings,
+        'crossing_ids'=crossing_ids,
+        'crossing_names'=crossings$Crossing,
+        'crossing_dists'=route$crossing_positions[[1]],
         'bounds'=st_bbox(segments),
         'id'=route$route_id
       )
     )
   }
-  
   route <- reactive({compileRoute(input$start_th, input$end_th)})
   
-  # Compile the crossings associated with that route
-  compileCrossings <- function(route_id) {
-    crossings <- crossing_positions %>%
-      filter(route_id==route_id) %>%
-      arrange(crossing_position)
-    crossing_geoms <- jmt_crossings_simplify %>% 
-      slice(crossings$crossing_id + 1)
-    return(
-      list(
-        'ids' = crossings$crossing_id,
-        'lin_refs' = crossings$crossing_position,
-        'geoms' = crossing_geoms
+  # reactive({print(route()$crossing_geoms$Crossing)})
+  
+  # Prepare a custom day selector UI based on the number of trip days
+  output$trip_day_selector <- renderUI({
+    fluidRow(
+      sliderTextInput(
+        inputId = 'trip_day',
+        label = "Select Crossing:",
+        choices = route()$crossing_names,
+        selected = route()$crossing_names[c(1)],
+        width = '100%',
+        grid = TRUE
       )
     )
-  }
-  crossings <- reactive(compileCrossings(route()$id))
+  })
+    
+  # Select raster data based on trip date
+  selectedData <- reactive({
+    map_date = input$start_date
+    selectedData <- data[[paste0(input$variable)]]
+    selectedData <- selectedData[[yday(map_date)]]
+    selectedData
+  })
+    
+  # trip_days <- as.numeric(difftime(input$end_date, input$start_date, units = "days")) + 1
+  # fluidRow(
+  #   sliderInput(
+  #     inputId = 'trip_day',
+  #     label = 'Trip Day:',
+  #     min = 1,
+  #     max = trip_days,
+  #     value = 1,
+  #     width = "100%",
+  #     step = 1
+  #   )
+  # )
+  # })
+  
+  # # Compile the crossings associated with that route
+  # compileCrossings <- function(route_id) {
+  #   crossings <- crossing_positions %>%
+  #     filter(route_id==route_id) %>%
+  #     arrange(crossing_position)
+  #   crossing_geoms <- jmt_crossings_simplify %>% 
+  #     slice(crossings$crossing_id + 1)
+  #   return(
+  #     list(
+  #       'ids' = crossings$crossing_id,
+  #       'lin_refs' = crossings$crossing_position,
+  #       'geoms' = crossing_geoms
+  #     )
+  #   )
+  # }
+  # crossings <- reactive(compileCrossings(route()$id))
   
   output$main_map <- renderLeaflet({
     pal <- colorNumeric(
@@ -488,12 +410,9 @@ server <- function(input, output) {
       
       # Map stream Crossings
       addMarkers(
-        # data = jmt_crossings_simplify,
-        data = route()$crossings, #################
-        # data = crossings()$ids,
-        # data = crossings()$geoms,
+        data = route()$crossing_geoms,
         label = ~htmlEscape(Crossing),
-        icon = ~crossingIcon,
+        icon = ~blueIcon,
         popup = ~htmlEscape(popup_field),
         group = "JMT Main Stream Crossings"
       ) %>%
@@ -510,7 +429,7 @@ server <- function(input, output) {
       
       # Map selected route
       addPolylines(
-        data = route()$segments,
+        data = route()$segment_geoms,
         color = "#126b20",
         label = ~htmlEscape(Name),
         weight = 6,
