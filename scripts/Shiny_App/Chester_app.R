051b481a65e6d975bd849a6d5a0172bbc5e738b9
Chester Harvey <chesterharvey@gmail.com>
2019-05-04 21:26:13 -0700

    red, yellow, and green icons

--- scripts/Shiny_App/Chester_app.R
+++ scripts/Shiny_App/Chester_app.R
@@ -14,6 +14,7 @@ library(htmltools)
 library(dplyr)
 library(markdown)
 library(sf)
+library(shinyWidgets)
 
 # ------------------------------- # 
 #         import data             # 
@@ -32,7 +33,7 @@ route_info <- readRDS("Data/route_info.rds")
 data <- list("snow_depth" = snow_depth_2015_jmt, 
              "precip" = precip_2015_jmt)
 
-# Make icon for stream crossings
+# Make icons for stream crossings
 crossingIcon <- makeIcon(
   iconUrl = "www/icon.png",
   iconRetinaUrl = "www/icon2x.png",
@@ -46,7 +47,58 @@ crossingIcon <- makeIcon(
   iconAnchorY = 35,
   shadowAnchorX = 6,
   shadowAnchorY = 32,
-  popupAnchorX = 6,
+  popupAnchorX = 1,
+  popupAnchorY = -35
+)
+
+crossingIconRed <- makeIcon(
+  iconUrl = "www/icon_red.png",
+  iconRetinaUrl = "www/icon2x_red.png",
+  shadowUrl = "www/icon_shadow.png",
+  # shadowRetinaUrl = "wwwn/icon2x_shadow.png",
+  iconHeight = 35,
+  iconWidth = 25,
+  shadowHeight = 35,
+  shadowWidth = 35,
+  iconAnchorX = 12,
+  iconAnchorY = 35,
+  shadowAnchorX = 6,
+  shadowAnchorY = 32,
+  popupAnchorX = 1,
+  popupAnchorY = -35
+)
+
+crossingIconYellow <- makeIcon(
+  iconUrl = "www/icon_yellow.png",
+  iconRetinaUrl = "www/icon2x_yellow.png",
+  shadowUrl = "www/icon_shadow.png",
+  # shadowRetinaUrl = "wwwn/icon2x_shadow.png",
+  iconHeight = 35,
+  iconWidth = 25,
+  shadowHeight = 35,
+  shadowWidth = 35,
+  iconAnchorX = 12,
+  iconAnchorY = 35,
+  shadowAnchorX = 6,
+  shadowAnchorY = 32,
+  popupAnchorX = 1,
+  popupAnchorY = -35
+)
+
+crossingIconGreen <- makeIcon(
+  iconUrl = "www/icon_green.png",
+  iconRetinaUrl = "www/icon2x_green.png",
+  shadowUrl = "www/icon_shadow.png",
+  # shadowRetinaUrl = "wwwn/icon2x_shadow.png",
+  iconHeight = 35,
+  iconWidth = 25,
+  shadowHeight = 35,
+  shadowWidth = 35,
+  iconAnchorX = 12,
+  iconAnchorY = 35,
+  shadowAnchorX = 6,
+  shadowAnchorY = 32,
+  popupAnchorX = 1,
   popupAnchorY = -35
 )
 
@@ -196,7 +248,7 @@ sidebar <- dashboardSidebar(
 
 main_map <- function(){
   leafletOutput(
-    "main_map",
+    "main_map"
   )
 }
 
@@ -267,8 +319,20 @@ server <- function(input, output) {
   output$trip_day_selector <- renderUI({
     trip_days <- as.numeric(difftime(input$end_date, input$start_date, units = "days")) + 1
     fluidRow(
+      # sliderTextInput(
+      #   inputId = 'trip_day',
+      #   label = 'Trip Day:',
+      #   choices = day_steps,
+      #   selected = "Stream A",
+      #   grid = TRUE,
+      #   # min = 1,
+      #   # max = trip_days,
+      #   # value = 1,
+      #   width = "100%"
+      #   # step = 1
+      # )
       sliderInput(
-        inputId = 'trip_day', 
+        inputId = 'trip_day',
         label = 'Trip Day:',
         min = 1,
         max = trip_days,
